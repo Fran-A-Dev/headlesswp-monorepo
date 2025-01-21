930702a8d1ca19017ec7b8c05809303c63d4fc85
Fran Agulto <francis.agulto@wpengine.com>
2025-01-21 13:49:44 -0600

    fixed nested folder structure

--- wp-content-files/bin/post-deploy.sh
+++ wp-content-files/bin/post-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Starting post deploy script..."
-echo "Switching to the wp-content-files directory..."
+echo "Switching to the correct directory..."
 cd /nas/content/live/mrpobackend/wp-content-files
 echo "Installing Composer dependencies..."
 composer install --optimize-autoloader --no-dev --no-progress
